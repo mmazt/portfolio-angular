2b39d8e6cab9b4b0599f64938dde08042bde5d74
mmazt <mazetto.milton@gmail.com>
2018-05-03 16:08:16 -0300

    Alterações nos icones de download e no componente de projetos

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public list = [
+    {
+      id: 'POPSCIENCE',
+      img: 'popscience1.jpg',
+      url: 'http://www.popscience.com.br'
+    },
+    {
+      id: 'OKNOTOKAY',
+      img: 'oknotokay1.jpg',
+      url: 'http://www.oknotokay.com'
+    },
+    {
+      id: 'PINEAPPLE',
+      img: 'pineapple1.jpg',
+      url: 'http://www.pineappleacessorios.com.br'
+    }
+  ];
+  public windowSize;
+  constructor() {
+    this.windowSize = {
+      height: window.innerHeight + 'px',
+      width: window.innerWidth + 'px'
+    };
   }
 
+  ngOnInit() {}
 }
